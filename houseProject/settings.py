e694e8065c115c69c2a4232b781600ea0c34a09d
follyamah <follyamah21@gmail.com>
2020-05-24 10:33:05 +0000

    Change Database to sqlite3

--- houseProject/settings.py
+++ houseProject/settings.py
@@ -88,15 +88,11 @@ CRISPY_TEMPLATE_BACK = 'bootstrap4'
 
 DATABASES = {
     'default': {
-    
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_house',
-        'USER': 'fofo',
-        'PASSWORD': 'rotciv8991',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
